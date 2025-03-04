246c6a636606f400e7b088f1fe5d0c70d329ccf5
zhouxingbang <zhouxingbang@yy.com>
2025-03-04 17:28:19 +0800

    fit: add some code.

--- packages/msfw-cli/src/lib/constants.ts
+++ packages/msfw-cli/src/lib/constants.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 
-export const MSFWNAME = 'msfw'
-
 const msfwPackageJson = require(`${path.resolve(__dirname, '../../')}/package.json`)
+
+export const MSFWNAME = 'msfw'
 
 export const MSFWVERSION = msfwPackageJson.version
